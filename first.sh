b28ef9d1b2c481422b01644eb232bb6726bd2441
apercov <apercov@home.at>
2020-02-24 23:36:00 +0200

    added at u16t1 hostname as variable to script

--- first.sh
+++ first.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+hst=$(cat /proc/sys/kernel/hostname);
 dt=$(date '+%d/%m/%Y %H:%M:%S');
-echo "first exec file created at u16t2 at $dt" >> first_exec.log
+echo "first exec file created at server $hst at $dt" >> first_exec.log
